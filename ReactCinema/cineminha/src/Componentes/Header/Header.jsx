dbceaede2322597b1a176d485126b7c8346fe1a0
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 12:57:42 -0300

    CineReact

---
+++ ReactCinema/cineminha/src/Componentes/Header/Header.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../NavBar/NavBar";
+
+function Header() {
+    return ( 
+        <header>
+            <span>Olá, vesitante!</span>
+            <h1>Bem-vindo ao MoviApp</h1>
+            <NavBar/>
+        </header>
+     );
+}
+export default Header;
