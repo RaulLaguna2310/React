7090eb22130cee5ee55afca5531df8371f1cd052
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-20 12:57:42 -0300

    cineminha

--- cineminha/src/Componentes/Header/Header.jsx
+++ cineminha/src/Componentes/Header/Header.jsx
@@ -2,7 +2,7 @@ import NavBar from "../NavBar/NavBar";
 
 function Header() {
     return ( 
-        <header>
+        <header className="flex justify-between bg-primary-raul text-white p-4">
             <span>Olá, vesitante!</span>
             <h1>Bem-vindo ao MoviApp</h1>
             <NavBar/>
--- cineminha/src/Pages/Home.jsx
+++ cineminha/src/Pages/Home.jsx
@@ -1,6 +1,28 @@
+import data from '../../artigos.json'
+
 function Home() {
     return ( 
-        <h1>Home</h1>
+        <main>
+            <input type="text" name="busca" id="busca" placeholder="Digite aqui sua busca" />
+            <div className='lista-filmes'>
+                {data.map(filme =>(
+                    <>
+                    <h1>{filme.title}</h1>
+                    <img src={filme.image}/>
+                    </>
+                ))}
+            </div>
+        </main>
      );
 }
 export default Home;
+
+/*<div className='card'>
+    <h1>Título do filme</h1>
+    <img src=""/>
+    <div className='tag'>
+        <span>Tags</span>
+    </div>
+    <p>Texto do post</p>
+</div>
+*/
