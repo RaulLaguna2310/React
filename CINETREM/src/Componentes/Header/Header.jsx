b2002e2a1840d23a83ad85672a464afff29cf7e5
RaulLaguna2310 <raullaguna2310@gmail.com>
2024-06-16 15:11:17 -0300

    CINETREM

--- CINETREM/src/Componentes/Header/Header.jsx
+++ CINETREM/src/Componentes/Header/Header.jsx
@@ -6,7 +6,10 @@ function Header() {
         <header className=" bg-color_amarelo1 text-color_branco p-3 rounded-br-lg">
             <div className="flex flex-row items-center">
                 <h1 className="text-6xl mt-3 mb-3 ml-3 text-color_branco font-medium tracking-widest">CINETREM</h1>
-                <input className="w-80 h-12 mt-3 rounded-2xl  flex justify-start mx-5 bg-color_preto placeholder:text-color_branco placeholder:text-sm pl-10" type="text" name="search" id="searchId" placeholder="Qual filme deseja buscar" />
+                <input
+                    className=" block w-80 h-12 rounded-2xl justify-start mx-5 mt-3 bg-color_preto placeholder:text-color_branco placeholder:text-sm pl-10 focus:outline-none"
+                    placeholder="Procure por um filme"
+                />
                 <div className="mt-3 ml-auto">
                     <Switch />
                 </div>
