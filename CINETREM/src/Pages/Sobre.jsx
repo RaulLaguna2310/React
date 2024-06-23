cc6107526935ce48de06f72c7eb23234379b911a
RaulLaguna2310 <raullaguna2310@gmail.com>
2024-06-22 21:02:06 -0300

    CINETREM

--- CINETREM/src/Pages/Sobre.jsx
+++ CINETREM/src/Pages/Sobre.jsx
@@ -2,7 +2,11 @@ function Sobre(){
 
     return(
         <>
-        <h1>sobre</h1>
+            <div className="bg-color_roxo3 p-6 pr-8 rounded-lg w-80 h-full flex flex-col drop-shadow-lg">
+                <p className="w-80 text-wrap">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur reprehenderit eaque pariatur molestias ut dolor deleniti deserunt quae libero voluptatem cum dignissimos debitis odit culpa laboriosam, nisi aliquam omnis dolores.
+                </p>
+            </div>
         </>
     )
 
